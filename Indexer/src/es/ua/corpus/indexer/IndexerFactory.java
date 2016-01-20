fa93e832a93299afc62bb74e398a30aa59f1d14b
paco <paco@paco-pc>
2016-01-20 09:20:49 +0100

    Remove ngrams. Improve speed.

--- Indexer/src/es/ua/corpus/indexer/IndexerFactory.java
+++ Indexer/src/es/ua/corpus/indexer/IndexerFactory.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package es.ua.corpus.indexer;
 
 import java.io.IOException;
@@ -13,36 +12,40 @@ import java.io.IOException;
  * @author paco
  */
 public class IndexerFactory {
-   
+
     private static IndexerFactory instance = null;
-    
+
     private IndexerFactory() {
     }
-    
+
     public static IndexerFactory getInstance() {
-        if ( instance == null ) {
+        if (instance == null) {
             instance = new IndexerFactory();
         }
-        
+
         return instance;
     }
-    
-    public Indexer getIndexer(String language, boolean lemma) throws IOException {
-        return this._create(language, Config.INDEXES_PATH, lemma);
+
+    public Indexer getIndexer(String language, boolean lemma, boolean title) throws IOException {
+        if (title) {
+            return this._create(language, Config.INDEXES_PATH + Config.FILE_SEPARATOR + "title" , lemma);
+        } else {
+            return this._create(language, Config.INDEXES_PATH, lemma);
+        }
     }
-    
+
     public Indexer getNGrammaIndexer(String language, int size, boolean lemma) throws IOException {
         return this._create(language, Config.INDEXES_PATH + Config.FILE_SEPARATOR + "ngramas" + Config.FILE_SEPARATOR + size, lemma);
     }
-    
+
     private Indexer _create(String language, String indexPath, boolean lemma) throws IOException {
-        if ( language.toLowerCase().equals("es") ) {
+        if (language.toLowerCase().equals("es")) {
             return new SpanishIndexer(indexPath, lemma);
-        } else if ( language.toLowerCase().equals("fr") ) {
+        } else if (language.toLowerCase().equals("fr")) {
             return new FrenchIndexer(indexPath, lemma);
         } else {
             return new EnglishIndexer(indexPath, lemma);
         }
     }
-    
+
 }
